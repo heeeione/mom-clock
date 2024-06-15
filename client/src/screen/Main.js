7263eaf8a956636ee9f3a9dd900b9c2b43359de9
heeeione <heewon410@naver.com>
2024-06-15 13:50:08 +0900

    feat: add alarm

--- client/src/screen/Main.js
+++ client/src/screen/Main.js
@@ -1,34 +1,30 @@
-import React, {useEffect, useState} from "react";
-import alarm_sound from "../assets/iphone_alarm.mp3"
+import React, { useEffect, useState } from 'react';
+import alarm_sound from '../assets/iphone_alarm.mp3';
 import useSound from 'use-sound';
-import AlarmList from "../Components/AlarmList";
-import {Modal,Box,Typography} from "@mui/material";
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
+import AlarmList from '../Components/AlarmList';
+import AddAlarm from '../Components/AddAlarm';
 
 const Main = () => {
-    const [play, {stop}] = useSound(alarm_sound);
-    const [openAlarmModal, setOpenAlarmModal] = React.useState(false)
-    const handleAlarmModal = () => setOpenAlarmModal(true)
-    const handleClose = () => setOpenAlarmModal(false)
+  const [play, { stop }] = useSound(alarm_sound);
+  const [openAlarmModal, setOpenAlarmModal] = React.useState(false);
+  const []
+  const handleAlarmModal = () => setOpenAlarmModal(true);
+  const handleClose = () => setOpenAlarmModal(false);
+  const handleSaveAlarm = (time) => {
+    const formattedAlarmTime = new Date(`1970-01-01T${time}:00`).toLocaleTimeString('ko-KR', {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+    setAlarmTimes([...alarmTimes, { time: formattedAlarmTime, active: true, idx: alarmTimes.length }]);
+  }
 
-    // 알람 리스트에는 저장된 알람을 불러와야 함
-    return (<React.Fragment>
-        <h1>알람</h1>
-        <button onClick={handleAlarmModal}>+</button>
-        <AddAlarm></AddAlarm>
-        <AlarmList >
-        </AlarmList>
-    </React.Fragment>);
-}
+  // 알람 리스트에는 저장된 알람을 불러와야 함
+  return (<React.Fragment>
+    <h1>알람</h1>
+    <button onClick={handleAlarmModal}>+</button>
+    <AddAlarm open={openAlarmModal} onClose={handleClose} onSave={handleSaveAlarm} />
+    <AlarmList>
+    </AlarmList>
+  </React.Fragment>);
+};
 export default Main;
