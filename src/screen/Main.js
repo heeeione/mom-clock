221a50532755cbbcad89ec444e77c80fa361dd88
heeeione <heewon410@naver.com>
2024-06-15 14:01:36 +0900

    feat: alarm list

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -7,7 +7,7 @@ import AddAlarm from '../Components/AddAlarm';
 const Main = () => {
   const [play, { stop }] = useSound(alarm_sound);
   const [openAlarmModal, setOpenAlarmModal] = React.useState(false);
-  const []
+  const [alarmTimes, setAlarmTimes] = useState([]);
   const handleAlarmModal = () => setOpenAlarmModal(true);
   const handleClose = () => setOpenAlarmModal(false);
   const handleSaveAlarm = (time) => {
@@ -16,15 +16,14 @@ const Main = () => {
       minute: '2-digit'
     });
     setAlarmTimes([...alarmTimes, { time: formattedAlarmTime, active: true, idx: alarmTimes.length }]);
-  }
+  };
 
   // 알람 리스트에는 저장된 알람을 불러와야 함
   return (<React.Fragment>
     <h1>알람</h1>
     <button onClick={handleAlarmModal}>+</button>
     <AddAlarm open={openAlarmModal} onClose={handleClose} onSave={handleSaveAlarm} />
-    <AlarmList>
-    </AlarmList>
+    <AlarmList alarmTimes={alarmTimes} setAlarmTimes={setAlarmTimes} />
   </React.Fragment>);
 };
 export default Main;
